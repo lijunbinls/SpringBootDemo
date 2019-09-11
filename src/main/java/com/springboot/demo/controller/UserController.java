68ad437f8dc5ae25a66602829867fc0e5798c2fc
lijunbinls <gamesOpens6@163.com>
2019-09-11 15:08:16 +0800

    thymeleaf,freemarker commit

--- src/main/java/com/springboot/demo/controller/UserController.java
+++ src/main/java/com/springboot/demo/controller/UserController.java
@@ -11,8 +11,13 @@ import com.springboot.demo.pojo.JSONResult;
 import com.springboot.demo.pojo.Resource;
 import com.springboot.demo.pojo.User;
 
+/**
+ * 
+ * @author 李先生
+ * @RestController = @ResponseBody + @Controller
+ *
+ */
 @RestController
-//@Controller
 @RequestMapping("/user")
 public class UserController {
 	
@@ -20,7 +25,6 @@ public class UserController {
 	private Resource resource;
 	
 	@RequestMapping("/getUserInfo")
-	//@ResponseBody
 	public JSONResult getUserInfo() {
 		
 		User user = new User();
@@ -32,14 +36,15 @@ public class UserController {
 		return JSONResult.ok(user);
 	}
 	
-	/*@RequestMapping("/getResource")
+	@RequestMapping("/getResource")
 	public JSONResult getResource() {
 		
 		Resource bean = new Resource();
 		BeanUtils.copyProperties(resource, bean);
 
 		return JSONResult.ok(bean);
-	}*/
+	}
+	
 	
 
 }
